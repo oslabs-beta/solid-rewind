e739098f5f91a69b5ebc4e7242c999293d8db2b1
Boblobblieb <RobertIGottlieb@gmail.com>
2022-12-15 17:31:42 -0500

    adding work on tree approach to devtool

---
+++ dev-tool-tree-approach/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+
+
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: {
+    port: 3005,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
