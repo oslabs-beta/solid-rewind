2eea0c8657a346416454306264a47bdfcf493dd9
Willem Rosenthal <willemrosenthal@gmail.com>
2023-01-02 16:38:48 -0500

    drag bar works

---
+++ rewind-chrome-extension/src/DragBar.jsx
@@ -0,0 +1,49 @@
+/* global chrome */
+import { createSignal } from 'solid-js';
+
+
+function DragBar(props) {
+
+  const [mouseListeners, setMouseListeners] = createSignal(false);
+  const [winSize, setWinSize] = createSignal(null);
+
+  const startDrag = () => {
+    // clear event listeners if they are still somehow there.
+    if (mouseListeners()) removeListeners();
+
+    // if mouseMove listener is not on, add it.
+    if (!mouseListeners()) {
+      setMouseListeners(true);
+      window.addEventListener('mousemove', handeMouseMove);
+      window.addEventListener('mouseup', handleMouseUp);
+    }
+  }
+
+  const handeMouseMove = (e) => {
+    const dragObj = document.querySelector('.'+ props.drag);
+
+    if (winSize() === null) {
+      setWinSize(dragObj.offsetHeight);
+    }
+
+    setWinSize(Math.max(0, winSize() + e.movementY));
+    dragObj.style.height = `${winSize()}px`;
+  }
+
+  const handleMouseUp = (e) => {
+    removeListeners();
+  }
+
+  const removeListeners = () => {
+    window.removeEventListener('mousemove', handeMouseMove);
+    window.removeEventListener('mouseup', handleMouseUp);
+    setMouseListeners(false);
+  }
+
+  return (
+    <div class='dragBar' onMouseDown={startDrag}>
+    </div>
+  );
+}
+
+export default DragBar;
